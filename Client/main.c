28a784a0a216784acba0fa50f52251b4f10ddede
geniusrabbit <U201613566@hust.edu.cn>
2017-08-28 10:24:58 +0800

    socket2.0

--- Client/main.c
+++ Client/main.c
@@ -13,7 +13,7 @@ int main() {
     struct sockaddr_in serv_addr;
     memset(&serv_addr,0,sizeof(serv_addr));//用0填充每个字节
     serv_addr.sin_family=AF_INET; //使用ipv4
-    serv_addr.sin_addr.s_addr=inet_addr("138.197.211.144");
+    serv_addr.sin_addr.s_addr=inet_addr("127.0.0.1");        //你将要连接的ip地址
     serv_addr.sin_port=htons(1234); //端口
     connect(sock,(struct sockaddr*)&serv_addr,sizeof(serv_addr));
 
